0069e2f677ce87a457a8400f34dba9aab7272da5
RSimas <ricardo.d.simas@ist.utl.pt>
2017-03-04 14:48:17 +0000

    Your Name seen

--- movie_list.c
+++ movie_list.c
@@ -34,7 +34,7 @@ int main(){
 	printf("%sThe Jungle Book (2016)\n", KGRN);
 	printf("%sThe Girl On The Train (2016)\n", KGRN);
 	printf("%sThe Great Wall (2016)\n", KRED);
-	printf("%sYour Name (2016)\n", KRED);
+	printf("%sYour Name (2016)\n", KGRN);
 
 	printf("\n\n%sMovies watched in 2016:\n\n", KNRM);
 
