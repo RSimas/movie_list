efead08781fde297f16e92bbb9b82c9b21bdcb15
RSimas <ricardo.d.simas@ist.utl.pt>
2017-07-25 22:49:52 +0100

    Valerian added, I'm so gonna regret this

--- movie_list.c
+++ movie_list.c
@@ -64,6 +64,7 @@ int main(){
 	printf("%sThor: Ragnarok (2017)\n", KRED);
 	printf("%sTwo For The Money (2005)\n", KGRN);
 	printf("%sUnknown (2011)\n", KGRN);
+	printf("%sValerian And The City Of A Thousand Planets (2017)\n", KRED);
 	printf("%sWar For The Planet Of The Apes (2017)\n", KGRN);
 	printf("%sWhile She Was Out (2008)\n", KGRN);
 	printf("%sWhy Him? (2016)\n", KGRN);
